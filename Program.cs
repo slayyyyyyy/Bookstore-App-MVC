62aa78a2b6628a5edfd9d350cf296b4ee02405dd
slayyyyyyy <andreea.gurzu02@gmail.com>
2023-04-09 20:38:26 +0300

    added Swagger UI

--- Program.cs
+++ Program.cs
@@ -2,10 +2,42 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Bookstore_App.Data;
 using Bookstore_App.Models;
+using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Options;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<Bookstore_AppContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Bookstore_AppContext") ?? throw new InvalidOperationException("Connection string 'Bookstore_AppContext' not found.")));
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Bookstore App",
+        Description = "An ASP.NET Core Web API for exploring our bookstore's products!",
+        TermsOfService = new Uri("https://localhost:7090/Home/Privacy"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+    // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -31,6 +63,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -44,3 +81,9 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
